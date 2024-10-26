565cb3ae85b5ab1065ef5a42f412e5e84be78b12
yvngmarti <ariasmartin.am@gmail.com>
2024-10-26 00:04:41 -0500

    Servicio findOne, findAll, remove, searchFile en InternFiles terminados

--- hrc-im-back/src/intern-files/intern-files.controller.ts
+++ hrc-im-back/src/intern-files/intern-files.controller.ts
@@ -10,6 +10,7 @@ import {
   UploadedFiles,
   ParseUUIDPipe,
   HttpCode,
+  Res,
 } from '@nestjs/common';
 import { InternFilesService } from './intern-files.service';
 import { CreateInternFileDto } from './dto/create-intern-file.dto';
@@ -19,16 +20,31 @@ import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import {
   ApiBearerAuth,
   ApiConsumes,
+  ApiOperation,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import {
   BAD_REQUEST,
   CREATE_RECORD,
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND,
+  READ_ALL_RECORDS,
+  READ_RECORD,
+  REMOVE_RECORD,
+  SUCCESSFUL_CREATION,
+  SUCCESSFUL_DELETION,
+  SUCCESSFUL_FETCH,
+  SUCCESSFUL_UPDATE,
   UNAUTHORIZED_ACCESS,
+  UPDATE_RECORD,
 } from 'src/common/constants/constants';
 import multerOptions from 'src/configs/multer.config';
 import { InternFile } from './entities/intern-file.entity';
+import { Public, UserRoles } from 'src/auth/decorators';
+import { UserRole } from 'src/common/enums';
+import { IApiResponse } from 'src/common/interfaces';
+import { Response } from 'express';
 
 @ApiTags('Intern Files')
 @ApiBearerAuth()
@@ -40,6 +56,7 @@ import { InternFile } from './entities/intern-file.entity';
 export class InternFilesController {
   constructor(private readonly internFilesService: InternFilesService) {}
 
+  @UserRoles(UserRole.ADMINISTRATOR, UserRole.SUPERVISOR_RH)
   @Post(':internId/upload')
   @HttpCode(201)
   @UploadInternFiles()
@@ -56,13 +73,18 @@ export class InternFilesController {
     ),
   )
   @ApiConsumes('multipart/form-data')
+  @ApiOperation({
+    summary: `${CREATE_RECORD} Only: ${UserRole.ADMINISTRATOR} and ${UserRole.SUPERVISOR_RH}`,
+  })
   @ApiResponse({
     status: 201,
     description: `${CREATE_RECORD} Insertion of practitioner files and response of safe paths.`,
     type: InternFile,
   })
   @ApiResponse({ status: 400, description: BAD_REQUEST })
+  @ApiResponse({ status: 500, description: INTERNAL_SERVER_ERROR })
   async uploadFiles(
+    @Param('internId', ParseUUIDPipe) internId: string,
     @UploadedFiles()
     files: {
       photo: Express.Multer.File[];
@@ -71,8 +93,7 @@ export class InternFilesController {
       birthCertificate: Express.Multer.File[];
       medicalInsurance: Express.Multer.File[];
     },
-    @Param('id', ParseUUIDPipe) id: string,
-  ) {
+  ): Promise<IApiResponse<any>> {
     // Agrupar todos los archivos en un solo array para trabajar con ellos
     const internFiles = [
       ...(files.photo || []),
@@ -83,35 +104,130 @@ export class InternFilesController {
     ];
 
     // Validamos y manejamos el rollback en caso de error
-    await this.internFilesService.validateAndHandleFiles(internFiles, id);
+    await this.internFilesService.validateAndHandleFiles(internId, internFiles);
 
     // console.log({ internFiles });
 
     // Si todo sale bien, continuamos con la insercion de las rutas de los archivos
-    return await this.internFilesService.create(internFiles, id);
+    const createdInternFiles = await this.internFilesService.create(
+      internId,
+      internFiles,
+    );
+    return { message: SUCCESSFUL_CREATION, data: createdInternFiles };
+  }
+
+  @Get()
+  @HttpCode(200)
+  @ApiOperation({
+    summary: `${READ_ALL_RECORDS} All users can access this endpoint`,
+  })
+  @ApiResponse({
+    status: 200,
+    description: SUCCESSFUL_FETCH,
+    type: [InternFile],
+  })
+  @ApiResponse({ status: 500, description: INTERNAL_SERVER_ERROR })
+  async findAll(): Promise<IApiResponse<any>> {
+    const allInternFiles = await this.internFilesService.findAll();
+    return {
+      message: SUCCESSFUL_FETCH,
+      data: allInternFiles,
+      records: allInternFiles.length,
+    };
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  @ApiOperation({
+    summary: `${READ_RECORD} All users can access this endpoint`,
+  })
+  @ApiResponse({
+    status: 200,
+    description: SUCCESSFUL_FETCH,
+    type: InternFile,
+  })
+  @ApiResponse({ status: 404, description: NOT_FOUND })
+  async findOne(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<IApiResponse<any>> {
+    const internFiles = await this.internFilesService.findOne(id);
+    return { message: SUCCESSFUL_FETCH, data: internFiles };
+  }
+
+  @Public()
+  @Get('intern-files/:internId/:fileName')
+  @HttpCode(200)
+  @ApiOperation({
+    summary: `${READ_RECORD} All users can access this endpoint`,
+  })
+  @ApiResponse({
+    status: 200,
+    description: SUCCESSFUL_FETCH,
+  })
+  @ApiResponse({ status: 404, description: NOT_FOUND })
+  searchFile(
+    @Res() res: Response,
+    @Param('internId') internId: string,
+    @Param('fileName') fileName: string,
+  ) {
+    const path = this.internFilesService.searchFile(internId, fileName);
+    res.sendFile(path);
+  }
+
+  @UserRoles(UserRole.ADMINISTRATOR, UserRole.SUPERVISOR_RH)
+  @Patch(':id/:internId')
+  @HttpCode(200)
+  @UploadInternFiles()
+  @ApiConsumes('multipart/form-data')
+  @ApiOperation({
+    summary: `${UPDATE_RECORD} Only: ${UserRole.ADMINISTRATOR} and ${UserRole.SUPERVISOR_RH}`,
+  })
+  @ApiResponse({
+    status: 200,
+    description: SUCCESSFUL_UPDATE,
+  })
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Param('internId', ParseUUIDPipe) internId: string,
+    @Body() updateInternFileDto: UpdateInternFileDto,
+    @UploadedFiles()
+    files: {
+      photo?: Express.Multer.File[];
+      curp?: Express.Multer.File[];
+      proofOfAddress?: Express.Multer.File[];
+      birthCertificate?: Express.Multer.File[];
+      medicalInsurance?: Express.Multer.File[];
+    },
+  ) {
+    return this.internFilesService.update(
+      id,
+      internId,
+      updateInternFileDto,
+      files,
+    );
+  }
+
+  @UserRoles(UserRole.ADMINISTRATOR, UserRole.SUPERVISOR_RH)
+  @Delete(':id/:internId')
+  @HttpCode(200)
+  @ApiOperation({
+    summary: `${REMOVE_RECORD} Only: ${UserRole.ADMINISTRATOR} and ${UserRole.SUPERVISOR_RH}`,
+  })
+  @ApiResponse({
+    status: 200,
+    description: `${REMOVE_RECORD} Delete all the practitioner's files.`,
+    type: InternFile,
+  })
+  @ApiResponse({ status: 404, description: NOT_FOUND })
+  @ApiResponse({ status: 500, description: INTERNAL_SERVER_ERROR })
+  async deleteFiles(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Param('internId', ParseUUIDPipe) internId: string,
+  ): Promise<IApiResponse<any>> {
+    const deletedInternFiles = await this.internFilesService.remove(
+      id,
+      internId,
+    );
+    return { message: SUCCESSFUL_DELETION, data: deletedInternFiles };
   }
 }
-// const elements = files;
-// for (let i = 0; i < files.length; i++)
-//   if (files[i].originalname === elements[i].originalname)
-//     throw new BadRequestException('El array contiene strings repetidos');
-
-// @Get()
-// findAll() {
-//   return this.internFilesService.findAll();
-// }
-
-// @Get(':id')
-// findOne(@Param('id') id: string) {
-//   return this.internFilesService.findOne(+id);
-// }
-
-// @Patch(':id')
-// update(@Param('id') id: string, @Body() updateInternFileDto: UpdateInternFileDto) {
-//   return this.internFilesService.update(+id, updateInternFileDto);
-// }
-
-// @Delete(':id')
-// async remove(@Param('id', ParseUUIDPipe) id: string) {
-//   return this.internFilesService.remove(id);
-// }
--- hrc-im-back/src/intern-files/intern-files.service.ts
+++ hrc-im-back/src/intern-files/intern-files.service.ts
@@ -1,5 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { CreateInternFileDto } from './dto/create-intern-file.dto';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UpdateInternFileDto } from './dto/update-intern-file.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { InternFile } from './entities/intern-file.entity';
@@ -8,6 +11,8 @@ import { InternsService } from 'src/interns/interns.service';
 import { checkForDuplicates, rollbackFiles } from './helpers';
 import { handleInternalServerError } from 'src/common/utils';
 import { ENV } from 'src/configs';
+import { join } from 'path';
+import { existsSync, rmSync, unlinkSync } from 'fs';
 
 @Injectable()
 export class InternFilesService {
@@ -17,14 +22,14 @@ export class InternFilesService {
     private readonly internsService: InternsService,
   ) {}
 
-  async create(files: Express.Multer.File[], internId: string) {
+  async create(internId: string, files: Express.Multer.File[]) {
     const intern = await this.internsService.findOne(internId);
 
-    const securePhotoUrl = `${ENV.SERVER_URL}/intern-files/${internId}/${files[0].filename}`;
-    const secureCurpUrl = `${ENV.SERVER_URL}/intern-files/${internId}/${files[1].filename}`;
-    const secureProofOfAddressUrl = `${ENV.SERVER_URL}/intern-files/${internId}/${files[2].filename}`;
-    const secureBirthCertificateUrl = `${ENV.SERVER_URL}/intern-files/${internId}/${files[3].filename}`;
-    const secureMedicalInsuranceUrl = `${ENV.SERVER_URL}/intern-files/${internId}/${files[4].filename}`;
+    const securePhotoUrl = `${ENV.HOST_API}/api/intern-files/${internId}/${files[0].filename}`;
+    const secureCurpUrl = `${ENV.HOST_API}/api/intern-files/${internId}/${files[1].filename}`;
+    const secureProofOfAddressUrl = `${ENV.HOST_API}/api/intern-files/${internId}/${files[2].filename}`;
+    const secureBirthCertificateUrl = `${ENV.HOST_API}/api/intern-files/${internId}/${files[3].filename}`;
+    const secureMedicalInsuranceUrl = `${ENV.HOST_API}/api/intern-files/${internId}/${files[4].filename}`;
 
     const newInternFiles = this.internFilesRepository.create({
       birthCertificate: secureBirthCertificateUrl,
@@ -43,7 +48,7 @@ export class InternFilesService {
     }
   }
 
-  async validateAndHandleFiles(files: Express.Multer.File[], internId: string) {
+  async validateAndHandleFiles(internId: string, files: Express.Multer.File[]) {
     try {
       // Valida que exactamente cinco archivos fueron subidos
       if (files.length !== 5)
@@ -62,19 +67,140 @@ export class InternFilesService {
       handleInternalServerError(error.message);
     }
   }
+
+  async findAll() {
+    try {
+      const allInternFiles = await this.internFilesRepository.find();
+      return allInternFiles;
+    } catch (error) {
+      handleInternalServerError(error.message);
+    }
+  }
+
+  async findOne(id: string) {
+    const existingInternFiles = await this.internFilesRepository.findOne({
+      where: { id },
+    });
+    if (!existingInternFiles)
+      throw new NotFoundException('Intern files not found.');
+    return existingInternFiles;
+  }
+
+  searchFile = (internId: string, fileName: string) => {
+    const path = join(
+      __dirname,
+      '../../',
+      `${ENV.INTERN_FILES_PATH}${internId}/${fileName}`,
+    );
+    console.log({ path });
+
+    if (!existsSync(path))
+      throw new BadRequestException(
+        `Not found file not with name: ${fileName}`,
+      );
+    return path;
+  };
+
+  async update(
+    id: string,
+    internId: string,
+    {
+      birthCertificate,
+      curp,
+      medicalInsurance,
+      photo,
+      proofOfAddress,
+    }: UpdateInternFileDto,
+    files: {
+      photo?: Express.Multer.File[];
+      curp?: Express.Multer.File[];
+      proofOfAddress?: Express.Multer.File[];
+      birthCertificate?: Express.Multer.File[];
+      medicalInsurance?: Express.Multer.File[];
+    },
+  ) {
+    const existingInternFiles = await this.findOne(id);
+    await this.internsService.findOne(internId);
+
+    let newSecurePhotoUrl: string = photo;
+    if (photo) {
+      newSecurePhotoUrl = `${ENV.HOST_API}/api/intern-files/${internId}/${files[0].filename}`;
+      const oldPhotoPath = join(
+        __dirname,
+        '../../',
+        ENV.INTERN_FILES_PATH,
+        existingInternFiles.photo.split('/').pop(),
+      );
+      if (existsSync(oldPhotoPath)) unlinkSync(oldPhotoPath);
+    }
+
+    let newSecureCurpUrl: string = curp;
+    if (curp) {
+      newSecureCurpUrl = `${ENV.HOST_API}/api/intern-files/${internId}/${files[1].filename}`;
+      const oldCurpPath = join(
+        __dirname,
+        '../../',
+        ENV.INTERN_FILES_PATH,
+        existingInternFiles.curp.split('/').pop(),
+      );
+      if (existsSync(oldCurpPath)) unlinkSync(oldCurpPath);
+    }
+
+    let newSecureProofOfAddressUrl: string = proofOfAddress;
+    if (proofOfAddress) {
+      newSecureProofOfAddressUrl = `${ENV.HOST_API}/api/intern-files/${internId}/${files[2].filename}`;
+      const oldProofOfAddressPath = join(
+        __dirname,
+        '../../',
+        ENV.INTERN_FILES_PATH,
+        existingInternFiles.proofOfAddress.split('/').pop(),
+      );
+      if (existsSync(oldProofOfAddressPath)) unlinkSync(oldProofOfAddressPath);
+    }
+
+    let newSecureBirthCertificateUrl: string = birthCertificate;
+    if (birthCertificate) {
+      newSecureBirthCertificateUrl = `${ENV.HOST_API}/api/intern-files/${internId}/${files[3].filename}`;
+      const oldBirthCertificatePath = join(
+        __dirname,
+        '../../',
+        ENV.INTERN_FILES_PATH,
+        existingInternFiles.birthCertificate.split('/').pop(),
+      );
+      if (existsSync(oldBirthCertificatePath))
+        unlinkSync(oldBirthCertificatePath);
+    }
+
+    let newSecureMedicalInsuranceUrl: string = medicalInsurance;
+    if (medicalInsurance) {
+      newSecureMedicalInsuranceUrl = `${ENV.HOST_API}/api/intern-files/${internId}/${files[4].filename}`;
+      const oldMedicalInsurancePath = join(
+        __dirname,
+        '../../',
+        ENV.INTERN_FILES_PATH,
+        existingInternFiles.medicalInsurance.split('/').pop(),
+      );
+      if (existsSync(oldMedicalInsurancePath))
+        unlinkSync(oldMedicalInsurancePath);
+    }
+
+    try {
+      const updatedInternFiles =
+        await this.internFilesRepository.save(existingInternFiles);
+      return updatedInternFiles;
+    } catch (error) {
+      handleInternalServerError(error.message);
+    }
+  }
+
+  async remove(id: string, internId: string) {
+    await this.findOne(id);
+    try {
+      const deletedInternFiles = await this.internFilesRepository.delete(id);
+      rollbackFiles(internId);
+      return deletedInternFiles.affected;
+    } catch (error) {
+      handleInternalServerError(error.message);
+    }
+  }
 }
-// findAll() {
-//   return `This action returns all internFiles`;
-// }
-
-// findOne(id: number) {
-//   return `This action returns a #${id} internFile`;
-// }
-
-// update(id: number, updateInternFileDto: UpdateInternFileDto) {
-//   return `This action updates a #${id} internFile`;
-// }
-
-// remove(id: number) {
-//   return `This action removes a #${id} internFile`;
-// }
