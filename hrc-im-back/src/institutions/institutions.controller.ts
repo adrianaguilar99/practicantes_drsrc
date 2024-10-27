71053286f2b43c035b6a5402a9c569444eccb24a
yvngmarti <ariasmartin.am@gmail.com>
2024-10-27 15:30:17 -0500

    Correccion en el nombre de propiedad de Auditorias en el modulo de Instituciones o Universidades

--- hrc-im-back/src/institutions/institutions.controller.ts
+++ hrc-im-back/src/institutions/institutions.controller.ts
@@ -174,8 +174,9 @@ export class InstitutionsController {
   @ApiResponse({ status: 500, description: INTERNAL_SERVER_ERROR })
   @HttpCode(200)
   @Delete()
-  async removeAll(): Promise<IApiResponse<any>> {
-    const removedInstitutions = await this.institutionsService.removeAll();
+  async removeAll(@Req() req): Promise<IApiResponse<any>> {
+    const user = req.user;
+    const removedInstitutions = await this.institutionsService.removeAll(user);
     return { message: SUCCESSFUL_DELETION, data: removedInstitutions };
   }
 }
