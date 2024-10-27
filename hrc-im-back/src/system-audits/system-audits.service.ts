cb731c1c0167f6cc53eca8d6c8e59ea624b66314
yvngmarti <ariasmartin.am@gmail.com>
2024-10-27 14:40:35 -0500

    Cambio de propiedad 'name' -> 'data'

--- hrc-im-back/src/system-audits/system-audits.service.ts
+++ hrc-im-back/src/system-audits/system-audits.service.ts
@@ -14,7 +14,7 @@ export class SystemAuditsService {
   async createSystemAudit(
     responsible: { id: string; fullName: string; role: string },
     action: string,
-    entity: { id: string; name: string },
+    entity: { id: string; data: string },
     status: string,
     errorMessage?: string,
   ) {
